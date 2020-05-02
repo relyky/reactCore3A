f1a98b4faa745b9a155db7cf2358765aa234095d
Rely Ky <relyky@hotmail.com>
2020-05-02 22:49:04 +0800

    tune

--- reactCore3A/Controllers/AccountController.cs
+++ reactCore3A/Controllers/AccountController.cs
@@ -91,24 +91,6 @@ namespace reactCore3A.Controllers
             return serializeToken;
         }
 
-        //private static byte[] SerializeToMemory(object o)
-        //{
-        //    MemoryStream stream = new MemoryStream();
-        //    IFormatter formatter = new BinaryFormatter();
-        //    formatter.Serialize(stream, o);
-        //    byte[] blob = stream.ToArray();
-        //    return blob;
-        //}
-
-        //private static T DeserializeFromMemory<T>(byte[] blob)
-        //{
-        //    MemoryStream stream = new MemoryStream(blob);
-        //    IFormatter formatter = new BinaryFormatter();
-        //    stream.Seek(0, SeekOrigin.Begin);
-        //    T obj = (T)formatter.Deserialize(stream);
-        //    return obj;
-        //}
-
         [HttpPost("[action]")]
         public IActionResult Login(LoginInfo login)
         {
